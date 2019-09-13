14f1ef40df263b948271e1f36c83f1b9133d4345
Tadeo <dsafsd>
2019-09-13 17:07:00 -0400

    asdf

--- src/htmlResponses.js
+++ src/htmlResponses.js
@@ -4,6 +4,12 @@ const page1 = fs.readFileSync(`${__dirname}/../client/client.html`);
 const page2 = fs.readFileSync(`${__dirname}/../client/client2.html`);
 const page3 = fs.readFileSync(`${__dirname}/../client/client3.html`);
 
+const getIndex = (request, response, index) => {
+  response.writeHead(200, { 'Content-Type': 'text/html' });
+  response.write(index);
+  response.end();
+};
+
 const getPageOne = (request, response) =>{
     getIndex(request, response, page1);
 };
@@ -16,11 +22,7 @@ const getPageThree = (request, response) => {
     getIndex(request, response, page3);
 };
 
-const getIndex = (request, response, index) => {
-  response.writeHead(200, { 'Content-Type': 'text/html' });
-  response.write(index);
-  response.end();
-};
+
 
 module.exports.getPageOne = getPageOne;
 module.exports.getPageTwo = getPageTwo;
